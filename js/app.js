aab185f1de42df91eee79e15722fd77b3b36c5b5
paulodavid5 <paulodaviddias4@gmail.com>
2022-10-07 12:19:22 +0100

    swiper.js + small upd

--- js/app.js
+++ js/app.js
@@ -1,95 +1,192 @@
 
-console.log(screen.width)
-// buttons active classe
-
-const btns = document.querySelectorAll('a');
-
-const callEvent = (elements) => {
-    elements.forEach((element) => {
-        element.addEventListener('click', (e) => {
-            btns.forEach(b => b.classList.remove('active'));
-            element.classList.add('active');
-        })
-    })
-}
-
-callEvent(btns);
-
-// on click button scroll to section
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        })
-    })
-})
-
-// ---------------------------------------->
-
-const sections = document.querySelectorAll('section')
-const links = document.querySelectorAll('a')
-const prevBtn = document.querySelector('#prev')
-const nextBtn = document.querySelector('#next')
-
-
-
-// //logic scroll
-var index = 0;
-const animationDurantion = 1000;
-
-nextBtn.addEventListener('click', () => {
-    index++;
-    sections.forEach((section, i) => {
-        if (i === index) {
-            section.scrollIntoView({ behavior: 'smooth' });
-        }
-    })
-})
-
-prevBtn.addEventListener('click', () => {
-    index--;
-    sections.forEach((section, i) => {
-        if (i === index) {
-            section.scrollIntoView({ behavior: 'smooth' });
-        }
-    })
-})
-
-const timeframe = 1000;
-
-
-function mouseSpeed() {
-    const lastTime = 0;
-    var now = new Date();
-    if (now - lastTime >= timeframe) {
-        lastTime = now;
-        console.log(lastTime)
-        return
-    }
-}
-
-
-window.addEventListener('wheel', (e) => {
-    mouseSpeed();
-    const delta = e.wheelDelta;
-    console.log(delta)
-    let currentTime = new Date().getTime();
-
-    if (currentTime - lastTime < animationDurantion) {
-        e.preventDefault();
-        return;
+var swiper = new Swiper(".swiper", {
+    // breakpoints: {
+    //     320: {
+    //         direction: "vertical",
+    //         slidesPerView: 1,
+    //         spaceBetween: 0,
+    //         mousewheel: true,
+    //         pagination: {
+    //             el: ".swiper-pagination",
+    //             clickable: true,
+    //         }
+    //     },
+    //     480: {
+    //         direction: "horizontal",
+    //         slidesPerView: 1,
+    //         spaceBetween: 0,
+    //         mousewheel: true,
+    //         pagination: {
+    //             el: ".swiper-pagination",
+    //             clickable: true,
+    //         }
+    //     }
+    // }
+    direction: "horizontal",
+    slidesPerView: 1,
+    spaceBetween: 0,
+    mousewheel: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     }
 
-    if (delta <= 0) {
-        const nextSection = new Event('click');
-        nextBtn.dispatchEvent(nextSection);
-
-    } else if (delta >= 0) {
-        const prevSection = new Event('click');
-        prevBtn.dispatchEvent(prevSection);
-    }
-    lastTime = currentTime;
 });
+
+// console.log(screen.width)
+// // buttons active classe
+// // -------------------------------------------->
+// // ---------------------------------------->
+// const btns = document.querySelectorAll('a');
+
+// const callEvent = (elements) => {
+//     elements.forEach((element) => {
+//         element.addEventListener('click', (e) => {
+//             btns.forEach(b => b.classList.remove('active'));
+//             element.classList.add('active');
+//         })
+//     })
+// }
+
+// callEvent(btns);
+// // -------------------------------------------->
+// // ---------------------------------------->
+// // on click button scroll to section
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         })
+//     })
+// })
+// // -------------------------------------------->
+// // ---------------------------------------->
+// window.location.reload("sections[1]")
+
+// const sections = document.querySelectorAll('section')
+// const links = document.querySelectorAll('a')
+// const prevBtn = document.querySelector('#prev')
+// const nextBtn = document.querySelector('#next')
+// const progressCircles = document.querySelectorAll('.circle')
+// const progressCircle = document.querySelector('.circle')
+
+// // ----------------------------
+// // ----------------------------
+// // ----------------------------
+// // ----------------------------
+// // PROGRESS CIRCLES LOGIC
+
+// const callEvent = (elements) => {
+//     elements.forEach((element) => {
+//         element.addEventListener('click', (e) => {
+//             progressCircles.forEach(c => c.classList.remove('active-circle'));
+//             element.classList.add('active-circle');
+//         })
+//     })
+// }
+
+// callEvent(progressCircles);
+
+// document.querySelectorAll('.circle[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         })
+//     })
+// })
+
+// ----------------------------
+// ----------------------------
+// ----------------------------
+// ----------------------------
+// END PROGRESS CIRCLES LOGIC
+
+// // //logic scroll
+// let index = 0;
+// // document.querySelector(`.circle-5`).style.backgroundColor = "#333333";
+// // const animationDurantion = 1000;
+
+// nextBtn.addEventListener('click', () => {
+//     index++;
+//     sections.forEach((section, i) => {
+//         if (i === index) {
+//             section.scrollIntoView({ behavior: 'smooth' });
+//             // document.querySelector(`.navbar_link-${index}`).classList.add('active');
+//             // document.querySelector(`.navbar_link-${index - 1}`).classList.remove('active');
+//             document.querySelector(`.circle-${i + 1}`).classList.add('active-circle');
+//             document.querySelector(`.circle-${i}`).classList.remove('active-circle');
+//         } else if (index > 5) {
+//             index = 0;
+//             document.querySelector(`.circle-${i + 6}`).classList.remove('active-circle');
+//             section.scrollIntoView({ left: '0', behavior: 'smooth' });
+//             document.querySelector(`.circle-${i + 1}`).classList.add('active-circle');
+//         }
+//     })
+// })
+
+// prevBtn.addEventListener('click', () => {
+//     index--;
+//     sections.forEach((section, i) => {
+//         if (i === index) {
+//             section.scrollIntoView({ behavior: 'smooth' });
+//             document.querySelector(`.circle-${i + 2}`).classList.remove('active-circle');
+//             document.querySelector(`.circle-${i + 1}`).classList.add('active-circle');
+//         } else if (index < 0) {
+//             index = 5;
+//             i = 5;
+//             document.querySelector(`.circle-1`).classList.remove('active-circle');
+//             document.querySelector(`.circle-${i + 1}`).classList.add('active-circle');
+//             // document.querySelector(`.circle-${i + 2}`).classList.remove('active-circle');
+//         }
+//     })
+// })
+
+// // const timeframe = 1000;
+
+
+
+
+// window.addEventListener('wheel', (e) => {
+
+//     if (e.deltaY > 0) {
+//         const nextSection = new Event('click');
+//         nextBtn.dispatchEvent(nextSection);
+
+//     } else if (e.deltaY < 0) {
+//         const prevSection = new Event('click');
+//         prevBtn.dispatchEvent(prevSection);
+//     }
+
+
+
+//     console.log(index);
+// });
+
+// var startY;
+// var swipeY = false;
+// var swiped = false;
+
+// document.addEventListener('touchmove', (e) => {
+//     e.preventDefault();
+//     startY = e.targetTouches[0].pageY;
+// }, false);
+
+// document.addEventListener('touchstart', (e) => {
+//     e.preventDefault();
+//     if (!swiped) {
+//         var diffY = e.changedTouches[0].pageY - startY;
+//         console.log(diffY);
+//         // check for final swipe condition
+//         if (diffY < 0) {
+//             const nextSection = new Event('click');
+//             nextBtn.dispatchEvent(nextSection);
+//         } else {
+//             const prevSection = new Event('click');
+//             prevBtn.dispatchEvent(prevSection);
+//         };
+//     };
+// }, false);
 
 
